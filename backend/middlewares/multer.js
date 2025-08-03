8f835ae4c37118b3492d7acaca0532069129cf60
fazal589 <fazaltab569@gmail.com>
2025-08-03 13:52:08 +0530

    Update multer.js

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,15 +1,22 @@
-import multer from "multer"
+import multer from "multer";
+import fs from "fs";
 
+// Ensure the folder exists at runtime
+const tempDir = "./tempUploads";
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + file.originalname;
+    cb(null, uniqueSuffix);
+  },
+});
 
-let storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"./public")
-    },
-    filename:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
-})
+const upload = multer({ storage });
 
-const upload=multer({storage})
-export default upload
+export default upload;
